84bdef7a71aead51b45d679298310e44d0fde017
Rick van Assen <rassen0134@student.landstede.nl>
2024-01-17 13:40:09 +0100

    feat: you can add a song and its being stored.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
-#[macro_use] extern crate rocket;
+#[macro_use]extern crate rocket;
+
+use rocket::serde::json::Json;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, PartialEq, Eq, Deserialize, Serialize, Clone)]
+struct Song {
+    artist: String,
+    title: String,
+    album: String,
+}
+
+static  mut SONGS: Vec<Song> = Vec::new();
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Json<Vec<Song>> {
+    Json(unsafe { SONGS.clone() })
+}
+
+#[post("/song", format = "json", data = "<song>")]
+fn post_song(song: Json<Song>) -> Json<Song> {
+    let song = song.0;
+    unsafe {
+        SONGS.push(song.clone());
+    }
+    Json(song)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, post_song])
 }
